07864af1a4c70449c2b612776d77392c1b5cb5bb
asdfv <grodno.by@mail.ru>
2017-01-24 23:29:58 +0300

    sdsd

--- 05-SpringHibernate/src/main/java/hib/Config.java
+++ 05-SpringHibernate/src/main/java/hib/Config.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
@@ -13,6 +14,7 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
+@EnableJpaRepositories
 public class Config {
 
 	@Bean
