ec20c855188b20b2da6f73f240a8d4e827f2d48d
asdfv <grodno.by@mail.ru>
2017-01-24 23:19:08 +0300

    hibernate-entitymanager

--- 05-SpringHibernate/src/main/java/hib/HibernateApp.java
+++ 05-SpringHibernate/src/main/java/hib/HibernateApp.java
@@ -2,7 +2,6 @@ package hib;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.data.repository.Repository;
 
 public class HibernateApp {
 	
