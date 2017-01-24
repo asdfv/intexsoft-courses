d57f4478f06c00cbd889a70156e8bf39d19a5465
asdfv <grodno.by@mail.ru>
2017-01-24 22:41:04 +0300

    Remove app.properties and other

--- 05-SpringHibernate/src/main/java/hib/Config.java
+++ 05-SpringHibernate/src/main/java/hib/Config.java
@@ -2,31 +2,22 @@ package hib;
 
 import javax.sql.DataSource;
 
-import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
-import org.springframework.core.env.Environment;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
-@ComponentScan({ "hib" })
-//@EnableJpaRepositories("hib")
 public class Config {
 
-	@Autowired
-	private Environment env;
-
-	@Bean("ds")
+	@Bean
 	public DataSource getDataSource() {
-		BasicDataSource dataSource = new BasicDataSource();
-		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
-		dataSource.setUrl(env.getProperty("jdbc.url"));
-		dataSource.setUsername(env.getProperty("jdbc.user"));
-		dataSource.setPassword(env.getProperty("jdbc.pass"));
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.postgresql.Driver");
+		dataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
+		dataSource.setUsername("postgres");
+		dataSource.setPassword("root");
 
 		return dataSource;
 	}
-
 }
