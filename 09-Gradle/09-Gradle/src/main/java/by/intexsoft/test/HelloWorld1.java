443cb05d26e0064ea11b32b63eeb5ba6cd282139
asdfv <grodno.by@mail.ru>
2017-02-09 17:36:08 +0300

    Gradle works

--- 09-Gradle/09-Gradle/src/main/java/by/intexsoft/test/HelloWorld1.java
+++ 09-Gradle/09-Gradle/src/main/java/by/intexsoft/test/HelloWorld1.java
@@ -3,11 +3,18 @@ package by.intexsoft.test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
+
 public class HelloWorld1 {
 
 	public static void main(String[] args) {
 		
-		Logger logger = LoggerFactory.getLogger("chapters.introduction.HelloWorld1");
-	    logger.debug("Hello world.");
+		Logger LOGGER = LoggerFactory.getLogger(HelloWorld1.class);
+	    LOGGER.debug("Hello world.");
+	    
+	    // print internal state
+	    LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+	    StatusPrinter.print(lc);
 	}
 }
